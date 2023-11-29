c684622bf7a97311b66e7b82890d81598d33171f
JesusGLDGIIM <65406164+JesusGLDGIIM@users.noreply.github.com>
2023-11-29 21:13:37 +0100

    Menu Comedor funcionales

--- Practica2/ProyectoUnityNPI/Assets/Scripts/ScriptComedor.cs
+++ Practica2/ProyectoUnityNPI/Assets/Scripts/ScriptComedor.cs
@@ -4,79 +4,120 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.Events;
 using TMPro;
+using System.IO;
 
 public class GeneradorBotones : MonoBehaviour
 {
-    [System.Serializable]   //Hacerlo serializable permite cambiar los valores de las instancias de esta clase desde el editor
-    public struct ButtonStruct{
+    [System.Serializable]
+    public struct ButtonStruct
+    {
         public string nombreDia;
-        public Sprite icon;
-        public UnityEvent onClick; //Almacena las funciones que se ejecutan al pulsar un boton
+        public string nombreTipoMenu;
+        public UnityEvent onClick;
     }
-    public GameObject botonPrefab; // Prefab del botón
-    public Transform contenedorBotones; // Contenedor para los botones
 
-    // Lista de información de botones serializables
-    private List<string> diasSemana = new List<string> { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" };
-    private List<string> tiposMenu = new List<string> { "Normal", "Vegetariano", "Celíaco" };
-    public List<ButtonStruct> listaDiasSemana;
-    public List<ButtonStruct> listaTiposMenu;
+    public GameObject botonPrefab;
+    public Transform contenedorBotones;
+
+    private List<string> diasSemana = new List<string> { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" };
+    private List<string> tiposMenu = new List<string> { "Normal", "Vegetariano", "Celiaco" };
+    private int tipoMenuSeleccionado = 0;
+    private int diaSemanaSeleccionado = 0;
+    public GameObject menu;
+
+    // Lista de objetos asociados a cada combinación de día y menú
+    public List<GameObject> objetosAsociados;
 
     void Start()
     {
-        // Generar los botones
+        Generarmenus();
         GenerarBotones();
     }
 
     void GenerarBotones()
     {
-        // Espaciado entre botones
         float espacioEntreBotones = 250f;
 
-        for(int i = 0; i < listaDiasSemana.Count; ++i){
-
-            //Creamos el botón, basándonos en el prefab
+        for (int i = 0; i < diasSemana.Count; ++i)
+        {
             GameObject nuevoBoton = Instantiate(botonPrefab, Vector3.zero, Quaternion.identity, transform);
 
-            //Calculamos su posición, y se la aplicamos
-            Vector2 buttonPosition = new Vector2(espacioEntreBotones*i - 650f, -1000f);
+            Vector2 buttonPosition = new Vector2(espacioEntreBotones * i * 1.20f - 750f, -1200f);
             nuevoBoton.GetComponent<RectTransform>().anchoredPosition = buttonPosition;
 
-            //Cambiamos el texto del botón
             nuevoBoton.GetComponentInChildren<TextMeshProUGUI>().text = diasSemana[i];
 
-            int diaIndex = i; // Variable local para capturar el valor correcto en el cierre
+            int diaIndex = i;
             nuevoBoton.GetComponent<Button>().onClick.AddListener(() => BotonClicDiaSemana(diaIndex));
-
         }
 
-        for(int i = 0; i < listaTiposMenu.Count; ++i){
-
-            //Creamos el botón, basándonos en el prefab
+        for (int i = 0; i < tiposMenu.Count; ++i)
+        {
             GameObject nuevoBoton = Instantiate(botonPrefab, Vector3.zero, Quaternion.identity, transform);
 
-            //Calculamos su posición, y se la aplicamos
-            Vector2 buttonPosition = new Vector2(espacioEntreBotones*i - 250f, 600f);
+            Vector2 buttonPosition = new Vector2(espacioEntreBotones * i * 1.20f - 350f, 400f);
             nuevoBoton.GetComponent<RectTransform>().anchoredPosition = buttonPosition;
 
-            //Cambiamos el texto del botón
             nuevoBoton.GetComponentInChildren<TextMeshProUGUI>().text = tiposMenu[i];
 
-            int menuIndex = i; // Variable local para capturar el valor correcto en el cierre
+            int menuIndex = i;
             nuevoBoton.GetComponent<Button>().onClick.AddListener(() => BotonClicTipoMenu(menuIndex));
-
         }
     }
 
-    // Función para manejar clics en los botones
     void BotonClicDiaSemana(int diaIndex)
     {
         Debug.Log("Se hizo clic en el botón del día: " + diasSemana[diaIndex]);
-        // Aquí puedes agregar la lógica que deseas realizar cuando se hace clic en un botón.
+
+        diaSemanaSeleccionado = diaIndex;
+
+        ActivarObjetoCorrespondiente();
     }
+
     void BotonClicTipoMenu(int menuIndex)
     {
         Debug.Log("Se hizo clic en el botón del tipo de menú: " + tiposMenu[menuIndex]);
-        // Aquí puedes agregar la lógica que deseas realizar cuando se hace clic en un botón.
+
+        tipoMenuSeleccionado = menuIndex;
+
+        ActivarObjetoCorrespondiente();
+    }
+
+    void ActivarObjetoCorrespondiente()
+    {
+        // Buscar el objeto asociado a la combinación de día y menú
+        string nombreObjeto = $"{diasSemana[diaSemanaSeleccionado]}_{tiposMenu[tipoMenuSeleccionado]}";
+        GameObject objetoAsociado = objetosAsociados.Find(obj => obj.name == nombreObjeto);
+
+        // Activar el objeto encontrado y desactivar los demás
+        foreach (var obj in objetosAsociados)
+        {
+            obj.SetActive(obj == objetoAsociado);
+        }
+    }
+
+    void Generarmenus()
+    {
+        // Asignar referencias a objetos asociados
+        objetosAsociados = new List<GameObject>();
+        for (int i = 0; i < diasSemana.Count; ++i)
+        {
+            for (int j = 0; j < tiposMenu.Count; ++j)
+            {
+                string nombreObjeto = $"{diasSemana[i]}_{tiposMenu[j]}";
+                GameObject objeto = GameObject.Find(nombreObjeto);
+
+                if (objeto != null)
+                {
+                    objetosAsociados.Add(objeto);
+                    objeto.SetActive(false);
+                }
+                else
+                {
+                    Debug.LogError($"Objeto {nombreObjeto} no encontrado.");
+                }
+            }
+        }
+        objetosAsociados[0].SetActive(true);
     }
 }
